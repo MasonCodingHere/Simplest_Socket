1445da5eb26ec54b626998501e2db6e119c24c48
mrxuxg <mrxuxg@gmail.com>
2021-05-14 15:01:42 +0800

    Add note

--- src/client.c
+++ src/client.c
@@ -1,24 +1,24 @@
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include <sys/socket.h> //socket系列函数头文件
+#include <arpa/inet.h> //sockaddr_in结构体、inet_pton函数和htons函数头文件
+#include <string.h> //strlen函数、memset函数头文件
+#include <unistd.h> //close函数头文件
+#include <stdlib.h> //exit函数头文件
+#include <stdio.h> //fgets函数、fputs函数、perror函数头文件
 
-const int BUFFER_SIZE = 1024;
-const char* server_ip = "127.0.0.1";
-const uint16_t SERVER_PORT = 2021;
+const int BUFFER_SIZE = 1024; //定义缓冲区大小
+const char* server_ip = "127.0.0.1"; //服务端IP地址
+const uint16_t SERVER_PORT = 2021; //服务端监听端口号
 
 int main(){
 	//定义服务端套接字地址结构
 	struct sockaddr_in server_sockaddr;
 	memset(&server_sockaddr, 0, sizeof(server_sockaddr));
-	server_sockaddr.sin_family = AF_INET;
+	server_sockaddr.sin_family = AF_INET; //指定IPv4
 	server_sockaddr.sin_port = htons(SERVER_PORT);
 	inet_pton(AF_INET, server_ip, &server_sockaddr.sin_addr);
 
 	//创建客户端套接字描述符
-	int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+	int client_fd = socket(AF_INET, SOCK_STREAM, 0); //指定TCP协议
 	if(client_fd < 0){
 		perror("socket");
 		exit(1);
@@ -33,14 +33,14 @@ int main(){
 	//开始数据传输
 	char sendbuf[BUFFER_SIZE];
 	char recvbuf[BUFFER_SIZE];
-	while(fgets(sendbuf, sizeof(sendbuf), stdin)){
-		if(strcmp(sendbuf, "Q\n") == 0)
+	while(fgets(sendbuf, sizeof(sendbuf), stdin)){ //从stdin读入待传输数据至sendbuf
+		if(strcmp(sendbuf, "Q\n") == 0) //输入Q表示退出
 			break;
-		send(client_fd, sendbuf, strlen(sendbuf), 0);
-		recv(client_fd, recvbuf, sizeof(recvbuf), 0);
-		fputs(recvbuf, stdout);
-		memset(sendbuf, 0, sizeof(sendbuf));
-		memset(recvbuf, 0, sizeof(recvbuf));
+		send(client_fd, sendbuf, strlen(sendbuf), 0); //将sendbuf中的数据通过client_fd套接字传输
+		recv(client_fd, recvbuf, sizeof(recvbuf), 0); //从client_fd套接字接收数据，保存至recvbuf
+		fputs(recvbuf, stdout);//将recvbuf中的数据输出至stdout
+		memset(sendbuf, 0, sizeof(sendbuf));//将sendbuf的内存值置0
+		memset(recvbuf, 0, sizeof(recvbuf));//将recvbuf的内存值置0
 	}
 
 	//关闭连接
--- src/server.c
+++ src/server.c
@@ -1,32 +1,32 @@
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <ctype.h>
+#include <sys/socket.h> //socket系列函数头文件
+#include <arpa/inet.h> //sockaddr_in结构体、inet_pton函数、htons函数头文件
+#include <string.h> //strlen函数、memset函数头文件
+#include <unistd.h> //close函数头文件
+#include <stdlib.h> //exit函数头文件
+#include <stdio.h> //perror函数头文件
+#include <ctype.h> //toupper函数头文件
 
-const char* server_ip = "127.0.0.1";
-const uint16_t SERVER_PORT = 2021;
-const int QUEUE = 1024;
-const int BUFFER_SIZE = 1024;
+const char* server_ip = "127.0.0.1"; //指定服务端IP地址
+const uint16_t SERVER_PORT = 2021;//指定监听端口号
+const int QUEUE = 1024; //用于listen函数第二个参数，指定内核应为相应套接字排队的最大连接数
+const int BUFFER_SIZE = 1024;//指定缓冲区大小
 
 int main(){
 	//定义服务端套接字地址结构并赋值
 	struct sockaddr_in server_sockaddr;
 	memset(&server_sockaddr, 0, sizeof(server_sockaddr));
-	server_sockaddr.sin_family = AF_INET;
+	server_sockaddr.sin_family = AF_INET; //指定IPv4
 	server_sockaddr.sin_port = htons(SERVER_PORT);
 	inet_pton(AF_INET, server_ip, &server_sockaddr.sin_addr);
 
 	//创建一个监听套接字描述符
-	int listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+	int listen_fd = socket(AF_INET, SOCK_STREAM, 0); //指定TCP协议
 	if(listen_fd < 0){
 		perror("socket");
 		exit(1);
 	}
 
-	//将套接字描述符绑定到套接字地址结构
+	//将监听套接字描述符绑定到套接字地址结构
 	if(bind(listen_fd, (struct sockaddr*) &server_sockaddr, sizeof(server_sockaddr)) < 0){
 		perror("bind");
 		exit(1);
@@ -38,7 +38,7 @@ int main(){
 		exit(1);
 	}
 
-	//接受连接请求
+	//接受连接请求，创建 已连接套接字描述符
 	int conn_fd = accept(listen_fd, NULL, NULL);
 	if(conn_fd < 0){
 		perror("accept");
@@ -52,12 +52,12 @@ int main(){
 		memset(recvbuf, 0, sizeof(recvbuf));
 		memset(sendbuf, 0, sizeof(sendbuf));
 		
-		recv(conn_fd, recvbuf, sizeof(recvbuf), 0);
-		if(strcmp(recvbuf, "Q\n") == 0)
+		recv(conn_fd, recvbuf, sizeof(recvbuf), 0); //从conn_fd套接字接收数据，保存至recvbuf
+		if(strcmp(recvbuf, "Q\n") == 0) //如果收到的数据是Q，表示退出
 			break;
-		fputs(recvbuf, stdout);
+		fputs(recvbuf, stdout); //将收到的数据原样输出至stdout
 	
-		//将客户端输入的字符串转为大写
+		//将recvbuf中的小写字母转为大写，新数据保存至sendbuf
 		for(int i = 0; i < strlen(recvbuf); ++i){
 			if(islower(recvbuf[i]))
 				sendbuf[i] = toupper(recvbuf[i]);
@@ -65,7 +65,7 @@ int main(){
 				sendbuf[i] = recvbuf[i];
 		}
 
-		send(conn_fd, sendbuf, strlen(sendbuf), 0);
+		send(conn_fd, sendbuf, strlen(sendbuf), 0); //将sendbuf中的数据通过conn_fd套接字传输
 		
 	}
 
