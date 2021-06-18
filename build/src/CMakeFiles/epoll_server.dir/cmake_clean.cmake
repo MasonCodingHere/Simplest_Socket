f98654e14fde420973e7f162165a355dcc67ab64
mrxuxg <mrxuxg@gmail.com>
2021-06-18 16:25:43 +0800

    build project

---
+++ build/src/CMakeFiles/epoll_server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../../bin/epoll_server"
+  "../../bin/epoll_server.pdb"
+  "CMakeFiles/epoll_server.dir/epoll_server.c.o"
+  "CMakeFiles/epoll_server.dir/epoll_server.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/epoll_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
